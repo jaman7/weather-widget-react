4798403fbee2e816ec76becb9dd4d623d1798ffa
jaman7 <krzysztofbalon123@gmail.com>
2024-10-30 23:11:33 +0100

    init

--- src/common/weather-widget/weatherWidget.model.tsx
+++ src/common/weather-widget/weatherWidget.model.tsx
@@ -1,3 +1,19 @@
+export interface IWeatherMain {
+  temp: number;
+}
+
+export interface IWeatherDescription {
+  description: string;
+  icon: string;
+}
+
+export interface IWeatherResponse {
+  id: number;
+  name: string;
+  main: IWeatherMain;
+  weather: IWeatherDescription[];
+}
+
 export interface IWeatherData {
   id?: number;
   city?: string;
